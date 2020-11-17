57a4b4043d8ec5c2aab83c57434ae34593f40bd8
Kevinkijak <70660384+Kevinkijak@users.noreply.github.com>
2020-11-16 18:53:57 -0600

    should work now.

---
+++ TileWriter.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class TileWriter {
+    public static void write(TilePanel app){
+        try {
+            File file = new File("slotMachine.bin");
+            ObjectOutputStream str = new ObjectOutputStream(new FileOutputStream(file, false));
+            str.writeObject(app);
+            str.close();
+            JOptionPane.showMessageDialog(null, "Tile Panels Saved!");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error writing to file", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
